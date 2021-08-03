4ee137d650ba5db7a89cc110c1cf7eeb6e48a066
stephane.malho <stepahen.malho@gmail.com>
2021-08-03 11:52:14 +0200

    add js elements

---
+++ assets/js/home.js
@@ -0,0 +1,44 @@
+import * as index from "./index";
+
+function getProducts(category){
+    return fetch('http://localhost:3000/api/' + category)
+    .then(response => response.json())
+    .then(datas => {
+        console.log(datas);
+        return datas;
+    })
+    .catch(error => {
+        console.log(error);
+        return error;
+    });
+}
+function renderProducts(products){
+    let container=document.getElementById("container");
+    let content="";
+    products.forEach(element => {
+        content+=
+        `
+        <article>
+          <a href="./pages/product.html?id=`+element._id+`">
+            <figure>
+              <img
+                src="`+element.imageUrl+`"
+                alt="appareil photo ancien avec zoom"
+              />
+              <span>`+index.priceToEuros(element.price)+`</span>
+              <figcaption>
+                <h3>`+element.name+`</h3>
+                <p>
+                  `+element.description+`
+                </p>
+              </figcaption>
+            </figure>
+          </a>
+        </article>
+        `     ;
+    });
+    container.innerHTML=content;
+}
+getProducts("cameras").then(result => {
+    renderProducts(result);
+});
