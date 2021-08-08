19b54a7f9225c2b3c9c39f4eeb173b2da5742850
stephane.malho <stepahen.malho@gmail.com>
2021-08-08 16:36:44 +0200

    add div container to fix error on w3C validator for the H2 title

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,45 +1,55 @@
 import * as index from "./index";
 
-function getProducts(category){
-    return fetch('http://localhost:3000/api/' + category)
-    .then(response => response.json())
-    .then(datas => {
-        console.log(datas);
-        return datas;
+function getProducts(category) {
+  return fetch("http://localhost:3000/api/" + category)
+    .then((response) => response.json())
+    .then((datas) => {
+      console.log(datas);
+      return datas;
     })
-    .catch(error => {
-        console.log(error);
-        return error;
+    .catch((error) => {
+      console.log(error);
+      return error;
     });
 }
-function renderProducts(products){
-    let container=document.getElementById("container");
-    let content="";
-    container.insertAdjacentHTML('beforebegin','<h2>Nos appareils photos</h2>');
-    products.forEach(element => {
-        content+=
-        `
+function renderProducts(products) {
+  let container = document.getElementById("container");
+  let content = "";
+  //container.insertAdjacentHTML("beforebegin", "<h2>Voici nos appareils photos</h2>");
+  products.forEach((element) => {
+    content +=
+      `
         <article>
-          <a href="./pages/product.html?id=`+element._id+`">
+          <a href="./pages/product.html?id=` +
+      element._id +
+      `">
             <figure>
               <img
-                src="`+element.imageUrl+`"
+                src="` +
+      element.imageUrl +
+      `"
                 alt="appareil photo ancien avec zoom"
               />
-              <span>`+index.priceToEuros(element.price)+`</span>
+              <span>` +
+      index.priceToEuros(element.price) +
+      `</span>
               <figcaption>
-                <h3>`+element.name+`</h3>
+                <h3>` +
+      element.name +
+      `</h3>
                 <p>
-                  `+element.description+`
+                  ` +
+      element.description +
+      `
                 </p>
               </figcaption>
             </figure>
           </a>
         </article>
         `;
-    });
-    container.innerHTML=content;
+  });
+  container.innerHTML = content;
 }
-getProducts("cameras").then(result => {
-    renderProducts(result);
+getProducts("cameras").then((result) => {
+  renderProducts(result);
 });
